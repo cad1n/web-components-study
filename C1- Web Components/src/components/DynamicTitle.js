9fb81c7eb90c0b94d44c487fc8ee80080af93ad7
Caio Augusto <caioaugusto4520@gmail.com>
2023-06-06 01:20:32 -0300

    Final modifications

--- C1- Web Components/src/components/DynamicTitle.js
+++ C1- Web Components/src/components/DynamicTitle.js
@@ -1,12 +1,28 @@
-class TituloDinamico extends HTMLElement{
-    constructor(){
-    super();
+class DynamicTitle extends HTMLElement{
+        constructor(){
+        super();
 
-    const shadow = this.attachShadow({mode : open})
+        const shadow = this.attachShadow({ mode : "open" })
 
-    //component base
+        //component base
+        const componentRoot = document.createElement("h1");
+        componentRoot.textContent = this.getAttribute("text")
 
-    //component style
-    
- }
+        //component style
+        const style = document.createElement("style");
+        style.textContent =
+        `
+        h1{
+            color: red;
+
+        }`;
+
+        //send to shadow DOM
+        shadow.appendChild(componentRoot);
+        shadow.appendChild(style);
+        
+    }
 }
+
+
+customElements.define("dynamic-title", DynamicTitle)
